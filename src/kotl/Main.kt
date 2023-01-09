41fc8272d8ca37a97a0befe5e92943bd09d5a6b4
Evgenii <evgeniikapchik@gmail.com>
2023-01-09 19:40:53 +0300

    1st lesson

--- src/kotl/Main.kt
+++ src/kotl/Main.kt
@@ -2,7 +2,53 @@ package kotl
 
 
 fun main(args: Array<String>) {
-    print(sum(4, 5))
+
+
+    val list = mutableListOf<String>()
+    list.add("one")
+    list.add("two")
+    list.addItem("one")
+    print(list.size)
+}
+
+private fun print(vararg args: String) {
+    for (string in args) {
+        println(string)
+    }
+}
+
+fun log(header: String, body: String = "", footer: String) {
+    if (header.isNotEmpty()) print(header)
+    if (body.isNotEmpty()) print(body)
 }
 
 private fun sum(a: Int, b: Int) = a + b
+
+class MyObject {
+    private val list = ArrayList<String>()
+
+    infix fun add(str: String) {
+        list.add(str)
+    }
+}
+
+fun <T> singletonList(`object`: T): List<T>{
+    val list = ArrayList<T>(1)
+    list.add(`object`)
+    return list
+}
+
+private tailrec fun printIt(str: String, times:Int){
+    if(times == 0)
+        println("done")
+    else {
+        println(str + times)
+        printIt(str, times -1)
+    }
+}
+
+fun <T> MutableList<T>.addItem(item:T){
+    if(!contains(item)){
+        add(item)
+    }
+}
