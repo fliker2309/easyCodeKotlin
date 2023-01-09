c5edda800c8102c386ffd7f6a6cf3f7333bfbb1c
Evgenii <evgeniikapchik@gmail.com>
2023-01-09 20:43:46 +0300

    3rd lesson

--- src/kotl/Main.kt
+++ src/kotl/Main.kt
@@ -5,42 +5,25 @@ import kotlin.reflect.jvm.internal.impl.load.java.structure.JavaClass
 
 fun main(args: Array<String>) {
 
-    var x : Int = 5
-    (x as? String)?.length
-
-
-
-}
-
-fun max (a:Int, b:Int) = if (a > b) a else b
-
-fun checkNumber(x:Int){
-  when(x){
-      in 1..100 -> println("positive")
-      in -100..-1 -> println("negative")
-      else -> println("out of range")
-
-  }
-}
-
-fun check(x:Any?) = when(x){
-    is String -> print("it's a string")
-    is String? -> print("it's a string or null")
-    else -> print("unknown type")
-}
-
-fun chechy(x:Any){
-    print((x as String).isEmpty())
-}
-
-fun convertGrade(grade:Int){
-    when(grade){
-        5-> println("a")
-        4-> println("b")
-        3-> println("c")
-        2,1-> println("d")
-
-        else -> throw IllegalArgumentException("unknown grade")
+val repository = object : Repository{
+    override fun getData(): Result {
+        TODO("Not yet implemented")
     }
+}
+    when(val result = repository.getData()){
+        is kotl.Result.Success -> print(result.data)
+        is kotl.Result.Error -> throw IllegalArgumentException(result.message)
+    }
+
+
+}
+
+sealed class Result{
+    class Success(val data:Int) : Result()
+    class Error(val message:String): Result()
+}
+
+interface Repository{
+    fun getData(): Result
 }
 
