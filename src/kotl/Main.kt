badede8a5e072cea50ca452341da4f3bc5d8e80c
Evgenii <evgeniikapchik@gmail.com>
2023-01-11 22:25:42 +0300

    learning interfaces

--- src/kotl/Main.kt
+++ src/kotl/Main.kt
@@ -4,24 +4,46 @@ import kotlin.reflect.jvm.internal.impl.load.java.structure.JavaClass
 
 
 fun main(args: Array<String>) {
+val x = C("Hello")
 
-    val array = arrayOf(1,"2", 3, "4")
-    array.forEachIndexed{ index,data ->
-        if(data is String)
-            print(index)
+print(x.doOne())
+
+}
+
+interface A {
+
+    val data: String
+    fun doOne() {
+        print(data)
     }
 
+}
+
+interface B {
+    val data:String
+    fun doOne() {
+        print(data+data)
+    }
+}
+
+class C(override val data:String): A,B{
+    override fun doOne() {
+        super<A>.doOne()
+        super<B>.doOne()
+        println("do one C $data")
+    }
 
 }
 
 
 
-sealed class Result{
-    class Success(val data:Int) : Result()
-    class Error(val message:String): Result()
+
+sealed class Result {
+    class Success(val data: Int) : Result()
+    class Error(val message: String) : Result()
 }
 
-interface Repository{
+interface Repository {
     fun getData(): Result
 }
 
